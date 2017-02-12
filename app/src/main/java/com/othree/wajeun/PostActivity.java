f66c2de946c7bb94b8bfd736397cde00829851e7
othreecodes <daviduchenna@outlook.com>
2017-02-12 18:56:19 +0100

    Added Post Activity

---
+++ app/src/main/java/com/othree/wajeun/PostActivity.java
@@ -0,0 +1,13 @@
+package com.othree.wajeun;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+    }
+}
